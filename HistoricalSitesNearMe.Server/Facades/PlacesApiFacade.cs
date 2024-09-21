061448adcf1df0c544fcff22a9980fa8c80a00eb
alexandergm1 <alexander.1gmac@gmail.com>
2024-09-21 16:28:12 +0100

    Changes to react frontend

--- HistoricalSitesNearMe.Server/Facades/PlacesApiFacade.cs
+++ HistoricalSitesNearMe.Server/Facades/PlacesApiFacade.cs
@@ -6,16 +6,18 @@ namespace HistoricalSitesNearMe.Server.Facades
     public class PlacesApiFacade
     {
         private readonly IHttpClientFactory httpClientFactory;
+        private readonly string apiKey;
 
-        public PlacesApiFacade(IHttpClientFactory httpClientFactory)
+        public PlacesApiFacade(IHttpClientFactory httpClientFactory, string apiKey)
         {
             this.httpClientFactory = httpClientFactory;
+            this.apiKey = apiKey;
         }
 
         public async Task<List<HistoricalSite>?> GetHistoricalSitesAsync()
         {
             using HttpClient httpClient = httpClientFactory.CreateClient("PlacesApi");
-            HttpResponseMessage response = await httpClient.GetAsync("/v2/places?categories=building.historic&filter=rect%3A10.716463143326969%2C48.755151258420966%2C10.835314015356737%2C48.680903341613316&limit=20&apiKey=c6a283e253fd44e5b25d836bd092a45a");
+            HttpResponseMessage response = await httpClient.GetAsync($"/v2/places?categories=building.historic&filter=rect%3A10.716463143326969%2C48.755151258420966%2C10.835314015356737%2C48.680903341613316&limit=20&apiKey={apiKey}");
             
             if (!response.IsSuccessStatusCode)
             {
--- historical-sites-near-me.client/src/App.tsx
+++ historical-sites-near-me.client/src/App.tsx
@@ -1,37 +1,24 @@
 "use client";
-
-import { useState } from "react";
+//import { useState } from "react";
 import {
   APIProvider,
   Map,
-  AdvancedMarker,
-  Pin,
-  InfoWindow,
+  // AdvancedMarker,
+  // Pin,
+  // InfoWindow,
 } from "@vis.gl/react-google-maps";
 
 export default function Intro() {
   const position = { lat: 53.54, lng: 10 };
-  const [open, setOpen] = useState(false);
 
   return (
-    <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}>
-      <div style={{ height: "100vh", width: "100%" }}>
-        <Map zoom={9} center={position} mapId={process.env.NEXT_PUBLIC_MAP_ID}>
-          <AdvancedMarker position={position} onClick={() => setOpen(true)}>
-            <Pin
-              background={"grey"}
-              borderColor={"green"}
-              glyphColor={"purple"}
-            />
-          </AdvancedMarker>
-
-          {open && (
-            <InfoWindow position={position} onCloseClick={() => setOpen(false)}>
-              <p>I'm in Hamburg</p>
-            </InfoWindow>
-          )}
-        </Map>
-      </div>
-    </APIProvider>
+    <>
+      <h1>Test</h1>
+      <APIProvider apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY!}>
+        <div style={{ height: "100vh", width: "100%" }}>
+          <Map zoom={9} center={position}></Map>
+        </div>
+      </APIProvider>
+    </>
   );
 }
