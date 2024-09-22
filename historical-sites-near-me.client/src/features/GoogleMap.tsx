dbdbf124ba1d739c3ae2b64888c65caf6991e172
alexandergm1 <alexander.1gmac@gmail.com>
2024-09-22 10:57:25 +0100

    Added routing and made map draggable

--- historical-sites-near-me.client/src/features/GoogleMap.tsx
+++ historical-sites-near-me.client/src/features/GoogleMap.tsx
@@ -1,5 +1,6 @@
-import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import { APIProvider, Map, AdvancedMarker } from "@vis.gl/react-google-maps";
 import { useParams } from "react-router-dom";
+import { mapStyles } from "../mapStyles";
 
 export default function GoogleMap() {
   const { lat, lng } = useParams();
@@ -11,7 +12,15 @@ export default function GoogleMap() {
       <h1>Test</h1>
       <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY!}>
         <div style={{ height: "100vh", width: "100%" }}>
-          <Map zoom={9} center={position}></Map>
+          <Map
+            defaultZoom={12}
+            defaultCenter={position}
+            gestureHandling={"greedy"}
+            mapId={import.meta.env.VITE_GOOGLE_MAPS_UNIQUE_ID!}
+            styles={mapStyles}
+          >
+            <AdvancedMarker position={position}></AdvancedMarker>
+          </Map>
         </div>
       </APIProvider>
     </>
